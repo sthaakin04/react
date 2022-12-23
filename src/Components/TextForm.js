8e3530f6a3aa89464e46b3b85b73156dc606c489
sthaakin04 <akinstha97@gmail.com>
2022-12-23 17:13:13 +0545

    added

---
+++ src/Components/TextForm.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const [text, setText] = useState('Enter text here')
+
+export default function TextForm(props) {
+  return (
+    <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" id="myBox" rows="8"></textarea>
+        </div>
+        <button className='btn btn-primary'>Convert To Uppercase</button>
+    </div>
+  )
+}
